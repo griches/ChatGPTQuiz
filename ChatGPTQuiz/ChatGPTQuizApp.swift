3676b412de0e618545007fd84c626637e0c679fa
Gary Riches <gary@bouncingball.mobi>
2025-06-17 21:06:42 +0100

    Initial Commit

---
+++ ChatGPTQuiz/ChatGPTQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTQuizApp.swift
+//  ChatGPTQuiz
+//
+//  Created by Gary Riches on 17/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
